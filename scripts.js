39ed9c61a2a17173217a4eacf78e42d9dfa05c15
QuantZero <yigitkasapoglu95@gmail.com>
2022-11-06 18:14:48 -0800

    Adding event listeners on add button and using v-model to push new members

--- scripts.js
+++ scripts.js
@@ -8,16 +8,35 @@ members = [
         fname: 'George',
         lname: 'Harrison',
         instrument: 'Electric Guitar'
+    },
+    {   
+        fname: 'Hurricane',
+        lname: 'Bassman',
+        instrument: 'Bass'
     }
-]
+
+    ]
 
 const handlingForms = {
 
     data() {
         return {
-            members: window.members
+            members: window.members,
+            newMember: {
+
+            }
         }
     },
+        methods: {
+            addMember: function() {
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+                this.members.push(this.newMember)
+                this.newMember = {};
+            } else {
+                alert('All fields must be filled.')
+            }
+        }
+    }
 
 };
 
